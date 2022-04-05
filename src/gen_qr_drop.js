6772f42572a813948c82b9fd4c181071434fcfbb
Mikhail Melnik <by.zumzoom@gmail.com>
2022-04-05 16:57:56 +0400

    update qr drop generator

--- src/gen_qr_drop.js
+++ src/gen_qr_drop.js
@@ -4,20 +4,22 @@ const { toBN } = require('../test/helpers/utils');
 const Wallet = require('ethereumjs-wallet').default;
 const { promisify } = require('util');
 const randomBytesAsync = promisify(require('crypto').randomBytes);
-const { ether } = require('@openzeppelin/test-helpers');
+const { ether, BN } = require('@openzeppelin/test-helpers');
 const qr = require('qr-image');
 const fs = require('fs');
+const { assert } = require('console');
 
 function keccak128 (input) {
     return keccak256(input).slice(0, 16);
 }
 
-const AMOUNT = ether('50');
-const COUNT = 1024;
+const AMOUNTS = [ether('5'), ether('15'), ether('25'), ether('50')];
+const COUNTS = [500, 300, 124, 100];
+
 const PREFIX = 'https://app.1inch.io/#/1/qr?';
 
-function makeDrop (wallets, amount) {
-    const elements = wallets.map((w, i) => w + toBN(amount).toString(16, 64));
+function makeDrop (wallets, amounts) {
+    const elements = wallets.map((w, i) => w + toBN(amounts[i]).toString(16, 64));
     const leaves = elements.map(keccak128).map(x => MerkleTree.bufferToHex(x));
     const tree = new MerkleTree(leaves, keccak128, { sortPairs: true });
     const root = tree.getHexRoot();
@@ -38,39 +40,43 @@ function uriEncode (b) {
 }
 
 function saveQr (i, url) {
+    // console.log(url);
     const code = qr.imageSync(url, { type: 'png' });
     fs.writeFileSync(`src/qr/${i}.png`, code);
 }
 
-// function verifyProof (wallet, amount, proof, root) {
-//     const tree = new MerkleTree([], keccak128, { sortPairs: true });
-//     const element = wallet + toBN(amount).toString(16, 64);
-//     const node = MerkleTree.bufferToHex(keccak128(element));
-//     console.log(tree.verify(proof, node, root));
-// }
+function verifyProof (wallet, amount, proof, root) {
+    const tree = new MerkleTree([], keccak128, { sortPairs: true });
+    const element = wallet + toBN(amount).toString(16, 64);
+    const node = MerkleTree.bufferToHex(keccak128(element));
+    // console.log(proof);
+    // console.log(node);
+    return tree.verify(proof, node, root);
+}
 
-// function uriDecode (s, root) {
-//     const b = Buffer.from(s.substring(PREFIX.length + 2).replace(/-/g, '+').replace(/_/g, '/').replace(/!/g, '='), 'base64');
-//     // const vBuf = b.slice(0, 1);
-//     const kBuf = b.slice(1, 17);
-//     const aBuf = b.slice(17, 29);
-//     let pBuf = b.slice(29);
-//
-//     const proof = [];
-//     while (pBuf.length > 0) {
-//         proof.push(pBuf.slice(0, 16));
-//         pBuf = pBuf.slice(16);
-//     }
-//
-//     const key = kBuf.toString('hex').padStart(64, '0');
-//     const wallet = Wallet.fromPrivateKey(Buffer.from(key, 'hex')).getAddressString();
-//     const amount = (new BN(aBuf.toString('hex'), 16)).toString();
-//
-//     verifyProof(wallet, amount, proof, root);
-// }
+function uriDecode (s, root) {
+    const b = Buffer.from(s.substring(PREFIX.length + 2).replace(/-/g, '+').replace(/_/g, '/').replace(/!/g, '='), 'base64');
+    // const vBuf = b.slice(0, 1);
+    // console.log(vBuf);
+    const kBuf = b.slice(1, 17);
+    const aBuf = b.slice(17, 29);
+    let pBuf = b.slice(29);
+
+    const proof = [];
+    while (pBuf.length > 0) {
+        proof.push(pBuf.slice(0, 16));
+        pBuf = pBuf.slice(16);
+    }
+
+    const key = kBuf.toString('hex').padStart(64, '0');
+    const wallet = Wallet.fromPrivateKey(Buffer.from(key, 'hex')).getAddressString();
+    const amount = new BN(aBuf.toString('hex'), 16).toString();
+
+    return verifyProof(wallet, amount, proof, root);
+}
 
 function genUrl (priv, amount, proof) {
-    const vBuf = Buffer.from([0]);
+    const vBuf = Buffer.from([3]);
     const kBuf = Buffer.from(priv.substring(32), 'hex');
     const aBuf = Buffer.from(toBN(amount).toString(16, 24), 'hex');
     const pBuf = Buffer.concat(proof.map(p => p.data));
@@ -80,24 +86,25 @@ function genUrl (priv, amount, proof) {
 }
 
 async function main () {
-    const privs = await genPrivs(COUNT);
-    // console.log(privs);
-
+    const privs = await genPrivs(COUNTS.reduce((s, a) => s + a, 0));
     const accounts = privs.map(p => Wallet.fromPrivateKey(Buffer.from(p, 'hex')).getAddressString());
-    // console.log(accounts);
-
-    const drop = makeDrop(accounts, AMOUNT);
-
-    // console.log(drop);
-    // console.log(drop.proofs[0]);
-
-    for (let i = 0; i < COUNT; i++) {
-        const url = genUrl(privs[i], AMOUNT, drop.proofs[i]);
-        console.log(url);
-        saveQr(i, url);
+    let amounts = [];
+    for (let i = 0; i < COUNTS.length; i++) {
+        amounts = amounts.concat(Array(COUNTS[i]).fill(AMOUNTS[i]));
     }
+    console.log('total:', amounts.length);
+    const drop = makeDrop(accounts, amounts);
 
-    // uriDecode(url, drop.root);
+    console.log(drop.root, amounts.reduce((acc, v) => acc.add(v), toBN('0')).toString());
+
+    for (let i = 0; i < amounts.length; i++) {
+        const url = genUrl(privs[i], amounts[i], drop.proofs[i]);
+        saveQr(i, url);
+        assert(uriDecode(url, drop.root));
+        // if (i % 200 == 0) {
+        //     console.log(url, uriDecode(url, drop.root));
+        // }
+    }
 }
 
 main();
