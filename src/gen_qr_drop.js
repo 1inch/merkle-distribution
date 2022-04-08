ebbb5f44704eb032015d228b7ae901a22c88dbd7
Mikhail Melnik <by.zumzoom@gmail.com>
2022-04-08 17:19:10 +0400

    linter

--- src/gen_qr_drop.js
+++ src/gen_qr_drop.js
@@ -8,7 +8,6 @@ const { ether, BN } = require('@openzeppelin/test-helpers');
 const qr = require('qr-image');
 const fs = require('fs');
 const { assert } = require('console');
-const { MAX_INT256 } = require('@openzeppelin/test-helpers/src/constants');
 
 function keccak128 (input) {
     return keccak256(input).slice(0, 16);
@@ -86,19 +85,18 @@ function genUrl (priv, amount, proof) {
     return PREFIX + 'd=' + baseArgs;
 }
 
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
+function shuffle (array) {
+    let currentIndex = array.length; let randomIndex;
 
     // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-
+    while (currentIndex !== 0) {
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
         // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+            array[randomIndex], array[currentIndex]];
     }
 
     return array;
