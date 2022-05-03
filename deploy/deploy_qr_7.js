0efab4eb755964a8c0cdaf3e739aa5a6e2360461
Mikhail Melnik <by.zumzoom@gmail.com>
2022-05-03 14:31:57 +0400

    new qr drop

---
+++ deploy/deploy_qr_7.js
@@ -0,0 +1,28 @@
+// const { assert } = require('chai');
+const hre = require('hardhat');
+const { getChainId } = hre;
+
+module.exports = async ({ deployments, getNamedAccounts }) => {
+    console.log('running deploy script');
+    console.log('network id ', await getChainId());
+
+    const { deploy } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    const args = ['0x111111111117dC0aa78b770fA6A738034120C302', '0x7466e5d06e5d6c72b47c76ff7b2a7090', 10];
+
+    const merkleDrop128 = await deploy('MerkleDrop128-7', {
+        contract: 'SignatureMerkleDrop128',
+        from: deployer,
+        args: args,
+    });
+
+    if (await getChainId() !== '31337') {
+        await hre.run('verify:verify', {
+            address: merkleDrop128.address,
+            constructorArguments: args,
+        });
+    }
+};
+
+module.exports.skip = async () => true;
