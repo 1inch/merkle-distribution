8257dfef333ff1b5b8d79dcecc7e5f697168ce81
Mikhail Melnik <by.zumzoom@gmail.com>
2022-09-14 13:29:52 +0400

    singapore drop

--- src/gen_qr_drop.js
+++ src/gen_qr_drop.js
@@ -22,10 +22,10 @@ function keccak128 (input) {
 // const AMOUNTS = [ether('1'), ether('20')];
 // const COUNTS = [10, 200];
 
-const AMOUNTS = [ether('1'), ether('20'), ether('40'), ether('50')];
-const COUNTS = [10, 100, 100, 100];
+const AMOUNTS = [ether('1'), ether('20'), ether('30'), ether('40'), ether('50')];
+const COUNTS = [10, 250, 350, 200, 200];
 
-const VERSION = 11;
+const VERSION = 12;
 
 const PREFIX = 'https://app.1inch.io/#/1/qr?';
 
