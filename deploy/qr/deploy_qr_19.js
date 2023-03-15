31e87848258b078013ae6081b236c19d61e50b67
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-03-15 11:42:42 +0400

    qr drop for paris party

--- deploy/qr/deploy_qr_19.js
+++ deploy/qr/deploy_qr_19.js
@@ -3,17 +3,17 @@ const hre = require('hardhat');
 const { getChainId } = hre;
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
-    console.log('running deploy script: deploy script 18');
+    console.log('running deploy script: deploy script 19');
     console.log('network id ', await getChainId());
 
     const { deploy } = deployments;
     const { deployer } = await getNamedAccounts();
 
     // 1inch address, merkle root, tree height
-    const args = ['0x111111111117dC0aa78b770fA6A738034120C302', '0xe3f3ec235a8935248757c0885181547c', 9];
+    const args = ['0x111111111117dC0aa78b770fA6A738034120C302', '0xf6a7118fa7e9d72a1acd63d62319e17d', 10];
 
     // change number
-    const merkleDrop128 = await deploy('MerkleDrop128-18', {
+    const merkleDrop128 = await deploy('MerkleDrop128-19', {
         contract: 'SignatureMerkleDrop128',
         from: deployer,
         args,
@@ -27,4 +27,4 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     }
 };
 
-module.exports.skip = async () => true;
+module.exports.skip = async () => false;
