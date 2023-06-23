6ec8cc48a9fa24d9cb6d79d988bb87e06d47760c
Edozie I <112929878+zodenode@users.noreply.github.com>
2023-06-23 12:43:17 +0400

    Update CumulativeMerkleDrop.sol
    Update Natspec

--- contracts/CumulativeMerkleDrop.sol
+++ contracts/CumulativeMerkleDrop.sol
@@ -9,24 +9,38 @@ import "@1inch/solidity-utils/contracts/libraries/SafeERC20.sol";
 
 import "./interfaces/ICumulativeMerkleDrop.sol";
 
+/// @title CumulativeMerkleDrop
+/// @dev This contract allows for claims of tokens based on a merkle tree.
 contract CumulativeMerkleDrop is Ownable, ICumulativeMerkleDrop {
     using SafeERC20 for IERC20;
     // using MerkleProof for bytes32[];
 
+    /// @notice The token to be claimed.
     address public immutable override token;
 
+    /// @notice The current merkle root for the claims tree.
     bytes32 public override merkleRoot;
+    /// @notice Tracks the cumulative amount claimed for each address.
     mapping(address => uint256) public cumulativeClaimed;
 
+    /// @param token_ The token to be claimed.
     constructor(address token_) {
         token = token_;
     }
 
+    /// @notice Sets a new merkle root for the claims tree.
+    /// @dev Only callable by the owner.
+    /// @param merkleRoot_ The new merkle root.
     function setMerkleRoot(bytes32 merkleRoot_) external override onlyOwner {
         emit MerkelRootUpdated(merkleRoot, merkleRoot_);
         merkleRoot = merkleRoot_;
     }
 
+    /// @notice Allows an account to claim an amount of the token.
+    /// @param account The address of the account making the claim.
+    /// @param cumulativeAmount The cumulative amount the account is claiming.
+    /// @param expectedMerkleRoot The expected current merkle root.
+    /// @param merkleProof The merkle proof needed to claim the tokens.
     function claim(
         address account,
         uint256 cumulativeAmount,
@@ -56,6 +70,10 @@ contract CumulativeMerkleDrop is Ownable, ICumulativeMerkleDrop {
     //     return merkleProof.verify(root, leaf);
     // }
 
+    /// @dev Verifies a merkle proof in assembly.
+    /// @param proof The merkle proof.
+    /// @param root The root of the merkle tree.
+    /// @param leaf The leaf being proven.
     function _verifyAsm(bytes32[] calldata proof, bytes32 root, bytes32 leaf) private pure returns (bool valid) {
         /// @solidity memory-safe-assembly
         assembly {  // solhint-disable-line no-inline-assembly
