bbfa0def4aa275cc6f74af5bc9840de2c6827692
Gleb Alekseev <alekseev.gleb@gmail.com>
2023-12-26 22:22:12 -0300

    qr drop for Davos

--- deploy/qr/deploy_qr_35.js
+++ deploy/qr/deploy_qr_35.js
@@ -4,7 +4,7 @@ const hre = require('hardhat');
 const { getChainId } = hre;
 
 // change number
-const VERSION = 34;
+const VERSION = 35;
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
     console.log('running deploy script: deploy script ' + VERSION.toString());
@@ -13,7 +13,7 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     const { deployer } = await getNamedAccounts();
 
     // 1inch address, merkle root, tree height
-    const args = ['0x111111111117dC0aa78b770fA6A738034120C302', '0xf2dcb647136e42bd8b1dc8a8f8a9346c', 9];
+    const args = ['0x111111111117dC0aa78b770fA6A738034120C302', '0xc8f9f70ceaa4d05d893e74c933eed42b', 9];
 
     const deployScriptName = 'MerkleDrop128-' + VERSION.toString();
     await deployAndGetContract({
